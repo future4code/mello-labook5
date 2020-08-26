9b5cfe78a23c330d679b1953fbf066d763fe238e
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-26 17:32:18 -0300

    alterando metodos

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -4,4 +4,4 @@ import { PostController } from "../controller/UserController"
 export const userRouter = express.Router()
 
 userRouter.post("/create", new PostController().createPost)
-userRouter.post("/feed", new PostController().getFeed)
+userRouter.get("/feed", new PostController().getFeed)
