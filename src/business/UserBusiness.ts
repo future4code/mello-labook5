85bc423ea0082322071d06f540ab9dc2a3e46c8f
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-25 21:05:35 -0300

    login

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -3,38 +3,52 @@ import { IdGenerator } from "../services/IdGenerator";
 import { HashManager } from "../services/HashManager";
 import { Authenticator } from "../services/Authenticator";
 
-
-
 export class UserBusiness {
-    public async signup(name: string, email: string, password: string): Promise<string> {
+	public async signup(
+		name: string,
+		email: string,
+		password: string
+	): Promise<string> {
+		if (!name || !email || !password) {
+			throw new Error("Please fill all the fields");
+		}
 
-        if (!name || !email || !password) {
-            throw new Error("Please fill all the fields");
-        }
+		if (email.indexOf("@") === -1) {
+			throw new Error("Invalid Email");
+		}
 
-        if(email.indexOf("@") === -1){
-            throw new Error("Invalid Email");
-        }
+		if (password.length < 6) {
+			throw new Error("Password must have at least 6 characters");
+		}
+
+		const id = IdGenerator.generate();
+		const hashPassword = HashManager.hash(password);
+
+		const userDB = new UserDatabase();
+		await userDB.registerUser(id, name, email, hashPassword);
+
+		const token = Authenticator.generateToken({ id });
+
+		return token;
+	}
+
+	public async login(email: string, password: string): Promise<string> {
+		const userDB = new UserDatabase();
+		const user = await userDB.getUserByEmail(email);
+
+		const correctPassword = await HashManager.compare(
+			password,
+			user.password
+		);
+
+		if (!correctPassword) {
+			throw new Error("Incorrect email or password");
+		}
+
+        const token = Authenticator.generateToken({ id: user.id });
         
-        if(password.length < 6){
-            throw new Error("Password must have at least 6 characters");
-        }
-
-        const id = IdGenerator.generate()
-        const hashPassword = HashManager.hash(password)
-
-        const userDB = new UserDatabase()
-        await userDB.registerUser(
-            id,
-            name,
-            email,
-            hashPassword
-        )
-
-        const token = Authenticator.generateToken({id})
-
         return token
     }
-
+    
 
 }
