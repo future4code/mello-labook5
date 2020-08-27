374346458e0e5dc9212659e2a36e4b1afbdbd806
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-27 00:32:39 -0300

    refatorando feed

--- src/data/FeedDatabase.ts
+++ src/data/FeedDatabase.ts
@@ -1,21 +1,42 @@
 import {BaseDatabase} from "./BaseDatabase";
 
 export class FeedDatabase extends BaseDatabase {
-  public async getFriendId(userId: string): Promise<any> {
+  public async getFriendsId(userId: string): Promise<any> {
     const result = await this.getConnection().raw(`
       SELECT user_friend_id
       FROM Friends_Relations
       WHERE user_id = "${userId}"
     `)
-
-    return result[0][0]
+    
+    return result[0]
   } 
 
-  public async getFeed(creatorPostId: string): Promise<any> {
+  // public async getFeed(creatorPostId: string): Promise<any> {
+  //   const result = await this.getConnection().raw(`
+  //     SELECT photo_url, description, created_at, type
+  //     FROM Posts
+  //     WHERE id = "${creatorPostId}"
+  //   `);
+
+  //   return result[0]
+  // }
+
+  public async getFeed(userId: string): Promise<any> {
     const result = await this.getConnection().raw(`
-      SELECT photo_url, description, created_at, type
-      FROM Posts
-      WHERE id = "${creatorPostId}"
+      SELECT 
+        p.id AS postId,
+        p.photo_url AS photoUrl,
+        p.description,
+        p.created_at AS createdAt,
+        p.type,
+        u.id AS creatorUserId,
+        u.name AS creatorUserName
+      FROM 
+        Posts p JOIN User_Info u ON p.user_id = u.id
+        JOIN Friends_Relations fr ON fr.user_friend_id = u.id
+      WHERE 
+        fr.user_id = "${userId}"
+      ORDER BY p.created_at DESC  
     `);
 
     return result[0]
